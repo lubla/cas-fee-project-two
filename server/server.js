af16f2740bf0c358031d347d8f5e88920fe18958
lubla <lubla@users.noreply.github.com>
2015-08-06 11:01:12 +0200

    Use mongodb in routes.

--- server/server.js
+++ server/server.js
@@ -3,25 +3,84 @@
  */
 var express = require('express');
 var logger = require('express-logger');
+var assert = require('assert');
 var path = require('path');
+var MongoClient = require('mongodb').MongoClient;
 
-var app = express();
+function setupServer(mongoDb) {
 
-var logFilePath = path.join(__dirname, 'server.log')
+    var app = express();
 
-app.use(logger({
-    path: logFilePath,
-    format: 'dev'
-}));
+    var logFilePath = path.join(__dirname, 'server.log');
+    var myLogger = logger({
+        path: logFilePath,
+        format: 'dev'
+    });
+
+    app.use(myLogger);
+
+    // Static files.
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    // Make the mongo db available in the routers.
+    app.use(function(req,res,next){
+        req.mongoDb = mongoDb;
+        next();
+    });
 
 
-var indexRoute = require('./routes/index');
+    // Routes.
+    var indexRoute = require('./routes/index');
+    app.use('/', indexRoute);
 
-app.use('/', indexRoute);
+    // Forward 404 to error handler to error handler.
+    app.use(function (req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
 
-var server = app.listen(3000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+    function errorHandler(err, req, res, next) {
+        if (res.headersSent) {
+            return next(err);
+        }
+
+        var errorInfo;
+        if (err instanceof Error) {
+            errorInfo = err.stack;
+        }
+        else {
+            errorInfo = err.toString();
+        }
+
+        console.log(errorInfo);
+        res.send(errorInfo.replace(/\n/g, '<br>'));
+    }
+
+    app.use(errorHandler);
+
+    // Start server.
+    var server = app.listen(3000, function () {
+        var host = server.address().address;
+        var port = server.address().port;
+
+        console.log('Example app listening at http://%s:%s', host, port);
+    });
+
+    // Close mongodb connection when server closes.
+    server.on('close', function(){
+        mongoDb.close();
+    });
+}
+
+
+var mongodbUrl = 'mongodb://localhost:27017/test';
+MongoClient.connect(mongodbUrl, function(err, mongoDb) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server.");
+    setupServer(mongoDb);
+//    db.close();
 });
+
+
