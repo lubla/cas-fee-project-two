3704d53cb2a1b0ea6a64f69bdcba17387f1d5485
lubla <lubla@users.noreply.github.com>
2015-08-31 10:19:10 +0200

    Frontend type interfaces added to store doodles.

--- frontend/app/interfaces/types.ts
+++ frontend/app/interfaces/types.ts
@@ -4,21 +4,92 @@
 module Home.Interfaces {
     "use strict";
 
+    /**
+     * Stores user data of the login view.
+     *
+     * @interface IUser
+     */
     export interface  IUser {
         email: string;
         password: string;
     }
 
+    /**
+     * Stores user data for the registration view.
+     *
+     * @interface IUserRegister
+     */
     export interface  IUserRegister {
         email: string;
         passwordHash: string;
     }
 
-
+    /**
+     * Stores user data of an registered user.
+     *
+     * @interface IUserProfile
+     */
     export interface  IUserProfile {
-        id : string;
+        id: string;
         email: string;
         passwordHash: string;
     }
 
+    /**
+     * Stores the data of a Doodle date proposal.
+     *
+     * @interface IDateProposal
+     */
+    export interface IDateProposal {
+        id: string;
+
+        /**
+         * The start date of the proposal.
+         */
+        start: Date;
+
+        /**
+         * The end date of the proposal.
+         */
+        end: Date;
+
+        /**
+         * The list of the names of the people that have accepted the proposal.
+         */
+        acceptedBy: Array<string>;
+    }
+
+    /**
+     * Stores the data of a Doodle.
+     *
+     * @interface IDoodle
+     */
+    export interface IDoodle {
+        id: string;
+
+        /**
+         * The id of the user that has created the Doodle.
+         */
+        ownerId: string;
+
+        /**
+         * The title of the Doodle.
+         */
+        title: string;
+
+        /**
+         * The place of the Doodle.
+         */
+        place: string;
+
+        /**
+         * The date proposals of the Doodle.
+         */
+        dateProposals: Array<IDateProposal>;
+
+        /**
+         * Indicates if the Doodle is expired.
+         */
+        isExpired: boolean;
+    }
 }
