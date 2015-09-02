ecaec17abb7b176b8bcbbc5aef9008d54bcf9b0e
lubla <lubla@users.noreply.github.com>
2015-09-02 15:11:02 +0200

    Date picker added, tests with locale service.

--- frontend/app/app-module.ts
+++ frontend/app/app-module.ts
@@ -1,16 +1,20 @@
 ///<reference path='../typings/tsd.d.ts' />
 module frontend {
-  'use strict';
+    'use strict';
 
-  /* @ngdoc object
-   * @name frontend
-   * @description
-   *
-   */
-  angular
-    .module('frontend', [
-      'ngRoute',
-      'mgcrea.ngStrap',
-      'home'
-    ]);
+    /* @ngdoc object
+     * @name frontend
+     * @description
+     *
+     */
+    angular
+        .module('frontend', [
+            'ngRoute',
+            'ngLocale',
+            'mgcrea.ngStrap',
+            'home',
+            'mgcrea.ngStrap.tooltip',
+            'mgcrea.ngStrap.helpers.dateParser',
+            'mgcrea.ngStrap.datepicker',
+        ]);
 }
--- frontend/app/home/controllers/nested-parent-controller.ts
+++ frontend/app/home/controllers/nested-parent-controller.ts
@@ -10,15 +10,19 @@ module Home.Controllers {
         // It provides $injector with information about dependencies to be injected into constructor
         // it is better to have it close to the constructor, because the parameters must match in count and type.
         // See http://docs.angularjs.org/guide/di
-        public static $inject = ['$log', '$location', '$http', 'Repository'];
+        public static $inject = ['$log', '$location', '$http', 'Repository', '$locale'];
 
         // dependencies are injected via AngularJS $injector
         constructor(private $log:ng.ILogService,
                     private $location:ng.ILocationService,
                     private $http:ng.IHttpService,
-                    private repository:Home.Interfaces.IRepository) {
+                    private repository:Home.Interfaces.IRepository,
+                    private $locale:ng.ILocaleService) {
+            $locale.id = "de-de";
             this.ctrlName = 'NestedParentCtrl';
         }
+
+        selectedDate:Date;
     }
 
 
