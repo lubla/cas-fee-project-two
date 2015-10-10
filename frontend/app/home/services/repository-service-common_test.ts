07a78b5b666062789165fcf6c4f16ca10c333637
lubla <lubla@users.noreply.github.com>
2015-10-10 11:01:53 +0200

    Frontend: Repository unit tests completed.

---
+++ frontend/app/home/services/repository-service-common_test.ts
@@ -0,0 +1,111 @@
+/**
+ * Created by Luzius on 09.10.2015.
+ */
+///<reference path='../../../typings/tsd.d.ts' />
+///<reference path='../../../app/interfaces/types.ts' />
+///<reference path='../../../app/interfaces/services.ts' />
+///<reference path='../../../app/utilities/md5.ts' />
+///<reference path='../../../app/utilities/uuid.ts' />
+
+module Home.UnitTestCommon {
+
+
+    export class RepositoryTest {
+
+
+        // Property values of user that is used in the tests.
+        static password = '11111';
+        static email = 'a@a.a';
+        static userId = Home.Utilities.Uuid.new();
+
+        // Property values of the doodle that is used in the test.
+        static place = 'place';
+        static title = 'title';
+
+        static user:Home.Interfaces.IUser = {
+            email: RepositoryTest.email,
+            password: RepositoryTest.password
+        };
+
+        static userProfile:Home.Interfaces.IUserProfile = {
+            _id: RepositoryTest.userId,
+            email: RepositoryTest.email,
+            passwordHash: Home.Utilities.Hash.MD5(RepositoryTest.password)
+        };
+
+        static userRegister:Home.Interfaces.IUserRegister = {
+            email: RepositoryTest.email,
+            passwordHash: Home.Utilities.Hash.MD5(RepositoryTest.password)
+        };
+
+        static doodle:Home.Interfaces.IDoodle;
+        static dateProposalCount = 2;
+
+        /**
+         * Initializes the test doodle.
+         */
+        static createDoodle(repository:Home.Interfaces.IRepository) {
+            RepositoryTest.doodle = repository.createNewDoodleSync(RepositoryTest.userId);
+            RepositoryTest.doodle.place = RepositoryTest.place;
+            RepositoryTest.doodle.title = RepositoryTest.title;
+            for (var dateProposal:number = 0; dateProposal < RepositoryTest.dateProposalCount; dateProposal++) {
+                RepositoryTest.doodle.addNewDateProposal();
+            }
+        }
+
+        /**
+         * Checks if a doodle is the test doodle.
+         *
+         * @param doodle The test doodle.
+         */
+        static expectDoodle(doodle) {
+            expect(doodle).toBeDefined();
+            expect(doodle.userId).toBe(RepositoryTest.userId);
+            expect(doodle.dateProposals.length).toBe(RepositoryTest.dateProposalCount);
+            expect(doodle.place).toBe(RepositoryTest.place);
+            expect(doodle.title).toBe(RepositoryTest.title);
+        }
+
+        static loginUser(repository:Home.Interfaces.IRepository, $httpBackend:ng.IHttpBackendService):void {
+            repository.login(RepositoryTest.user);
+            $httpBackend.flush();
+        }
+
+
+        static setupRepositoryBackend($httpBackend:ng.IHttpBackendService):void {
+            // http backend definition for the post doodle request.
+            $httpBackend.when('POST', '/postDoodle', RepositoryTest.doodle)
+                .respond(RepositoryTest.doodle);
+
+            // http backend definition for the put doodle request.
+            $httpBackend.when('PUT', '/putDoodle', RepositoryTest.doodle)
+                .respond(RepositoryTest.doodle);
+
+            // http backend definition for the get doodle request.
+            $httpBackend.when('GET', '/getDoodle?doodleId=' + RepositoryTest.doodle._id)
+                .respond(RepositoryTest.doodle);
+
+            // http backend definition for the get doodles for user request.
+            $httpBackend.when('GET', '/getDoodlesForUser?userId=' + RepositoryTest.userId)
+                .respond([RepositoryTest.doodle, RepositoryTest.doodle, RepositoryTest.doodle]);
+
+            // http backend definition for the get doodle for register request.
+            $httpBackend.when('GET', '/getDoodleRegister?registerId=' + RepositoryTest.doodle.registerId)
+                .respond(RepositoryTest.doodle);
+
+            // http backend definition for the delete doodle request.
+            $httpBackend.when('DELETE', '/deleteDoodle?doodleId=' + RepositoryTest.doodle._id)
+                .respond(true);
+
+            // http backend to register a user.
+            $httpBackend.when('POST', '/registerUser', RepositoryTest.userRegister)
+                .respond(RepositoryTest.userProfile);
+
+            // http backend definition for the get user profile request.
+            $httpBackend.when('GET', '/getUserProfiles?email=' + RepositoryTest.user.email + '&passwordHash=' + Home.Utilities.Hash.MD5(RepositoryTest.user.password))
+                .respond([RepositoryTest.userProfile]);
+
+        }
+
+    }
+}
--- frontend/app/home/services/repository-service_test.ts
+++ frontend/app/home/services/repository-service_test.ts
@@ -3,6 +3,7 @@
 ///<reference path='../../../app/interfaces/services.ts' />
 ///<reference path='../../../app/utilities/md5.ts' />
 ///<reference path='../../../app/utilities/uuid.ts' />
+///<reference path='repository-service-common_test.ts' />
 
 /* global describe, beforeEach, it, expect, inject, module */
 
@@ -10,67 +11,8 @@
 
 describe('Repository', function () {
     var repository:Home.Interfaces.IRepository;
-    var $httpBackend;
+    var $httpBackend: ng.IHttpBackendService;
     var $rootScope;
-
-    // Property values of user that is used in the tests.
-    var password = '11111';
-    var email = 'a@a.a';
-    var userId = Home.Utilities.Uuid.new();
-
-    // Property values of the doodle that is used in the test.
-    var place = 'place';
-    var title = 'title';
-    var dateProposalCount = 2;
-
-    var user:Home.Interfaces.IUser = {
-        email: email,
-        password: password
-    };
-
-    var userProfile:Home.Interfaces.IUserProfile = {
-        _id: userId,
-        email: email,
-        passwordHash: Home.Utilities.Hash.MD5(password)
-    };
-
-    var userRegister:Home.Interfaces.IUserRegister = {
-        email: email,
-        passwordHash: Home.Utilities.Hash.MD5(password)
-    };
-
-    var doodle:Home.Interfaces.IDoodle;
-
-    /**
-     * Initializes the test doodle.
-     */
-    function createDoodle() {
-        doodle = repository.createNewDoodleSync(userId);
-        doodle.place = place;
-        doodle.title = title;
-        for (var dateProposal:number = 0; dateProposal < dateProposalCount; dateProposal++) {
-            doodle.addNewDateProposal();
-        }
-    }
-
-    /**
-     * Checks if a doodle is the test doodle.
-     *
-     * @param doodle The test doodle.
-     */
-    function expectDoodle(doodle) {
-        expect(doodle).toBeDefined();
-        expect(doodle.userId).toBe(userId);
-        expect(doodle.dateProposals.length).toBe(dateProposalCount);
-        expect(doodle.place).toBe(place);
-        expect(doodle.title).toBe(title);
-    }
-
-
-    function loginUser():void {
-        repository.login(user);
-        $httpBackend.flush();
-    }
 
     beforeEach(module('home'));
 
@@ -81,41 +23,9 @@ describe('Repository', function () {
         repository = $injector.get('Repository');
         $rootScope = $injector.get('$rootScope');
 
-        createDoodle();
+        Home.UnitTestCommon.RepositoryTest.createDoodle(repository);
 
-        // http backend definition for the post doodle request.
-        $httpBackend.when('POST', '/postDoodle', doodle)
-            .respond(doodle);
-
-        // http backend definition for the put doodle request.
-        $httpBackend.when('PUT', '/putDoodle', doodle)
-            .respond(doodle);
-
-        // http backend definition for the get doodle request.
-        $httpBackend.when('GET', '/getDoodle?doodleId=' + doodle._id)
-            .respond(doodle);
-
-        // http backend definition for the get doodles for user request.
-        $httpBackend.when('GET', '/getDoodlesForUser?userId=' + userId)
-            .respond([doodle, doodle, doodle]);
-
-        // http backend definition for the get doodle for register request.
-        $httpBackend.when('GET', '/getDoodleRegister?registerId=' + doodle.registerId)
-            .respond(doodle);
-
-        // http backend definition for the delete doodle request.
-        $httpBackend.when('DELETE', '/deleteDoodle?doodleId=' + doodle._id)
-            .respond(true);
-
-        // http backend to register a user.
-        $httpBackend.when('POST', '/registerUser', userRegister)
-            .respond(userProfile);
-
-        // http backend definition for the get user profile request.
-        $httpBackend.when('GET', '/getUserProfiles?email=' + user.email + '&passwordHash=' + Home.Utilities.Hash.MD5(user.password))
-            .respond([userProfile]);
-
-
+        Home.UnitTestCommon.RepositoryTest.setupRepositoryBackend($httpBackend);
     }));
 
     it('should equal Repository', function () {
@@ -123,37 +33,37 @@ describe('Repository', function () {
     });
 
     it('login should set loggedInUser and return the user profile of the user', function () {
-        var result = repository.login(user);
+        var result = repository.login(Home.UnitTestCommon.RepositoryTest.user);
         result.then(userProfile => {
             expect(repository.loggedInUser).not.toBe(null);
             expect(userProfile).toBeDefined();
-            expect(userProfile.passwordHash).toBe(Home.Utilities.Hash.MD5(password));
-            expect(userProfile.email).toBe(email);
+            expect(userProfile.passwordHash).toBe(Home.Utilities.Hash.MD5(Home.UnitTestCommon.RepositoryTest.password));
+            expect(userProfile.email).toBe(Home.UnitTestCommon.RepositoryTest.email);
         });
         $httpBackend.flush();
     });
 
     it('register user should set loggedInUser and return the profile of the user', function () {
-        var result = repository.registerUser(user);
+        var result = repository.registerUser(Home.UnitTestCommon.RepositoryTest.user);
         result.then(userProfile => {
             expect(repository.loggedInUser).not.toBe(null);
             expect(userProfile).toBeDefined();
-            expect(userProfile.passwordHash).toBe(Home.Utilities.Hash.MD5(password));
-            expect(userProfile.email).toBe(email);
+            expect(userProfile.passwordHash).toBe(Home.Utilities.Hash.MD5(Home.UnitTestCommon.RepositoryTest.password));
+            expect(userProfile.email).toBe(Home.UnitTestCommon.RepositoryTest.email);
         });
         $httpBackend.flush();
     });
 
     it('create new doodle should return an empty doodle', function (done) {
 
-        loginUser();
+        Home.UnitTestCommon.RepositoryTest.loginUser(repository, $httpBackend);
 
         // Note repository.createNewDoodle is async but not using $http =>
         // use the done() function (to indicate that the callback is called) together $rootScope.$apply() (to pump to event loop).
         var result = repository.createNewDoodle();
         result.then(doodle => {
             expect(doodle).toBeDefined();
-            expect(doodle.userId).toBe(userId);
+            expect(doodle.userId).toBe(Home.UnitTestCommon.RepositoryTest.userId);
             expect(doodle.dateProposals.length).toBe(0);
             expect(doodle.place).toBe('');
             expect(doodle.title).toBe('');
@@ -165,42 +75,42 @@ describe('Repository', function () {
 
     it('post doodle should return the posted doodle (i.e. the test doodle)', function () {
         repository
-            .postDoodle(doodle)
-            .then(doodle => expectDoodle(doodle));
+            .postDoodle(Home.UnitTestCommon.RepositoryTest.doodle)
+            .then(doodle => Home.UnitTestCommon.RepositoryTest.expectDoodle(doodle));
 
         $httpBackend.flush();
     });
 
     it('put doodle should return the updated doodle (i.e. the test doodle)', function () {
         repository
-            .putDoodle(doodle)
-            .then(doodle => expectDoodle(doodle));
+            .putDoodle(Home.UnitTestCommon.RepositoryTest.doodle)
+            .then(doodle => Home.UnitTestCommon.RepositoryTest.expectDoodle(doodle));
 
         $httpBackend.flush();
     });
 
     it('get doodle should return the test doodle', function () {
         repository
-            .getDoodle(doodle._id)
-            .then(doodle => expectDoodle(doodle));
+            .getDoodle(Home.UnitTestCommon.RepositoryTest.doodle._id)
+            .then(doodle => Home.UnitTestCommon.RepositoryTest.expectDoodle(doodle));
 
         $httpBackend.flush();
     });
 
     it('get doodle for register should return the test doodle', function () {
         repository
-            .getDoodleRegister(doodle.registerId)
-            .then(doodle => expectDoodle(doodle));
+            .getDoodleRegister(Home.UnitTestCommon.RepositoryTest.doodle.registerId)
+            .then(doodle => Home.UnitTestCommon.RepositoryTest.expectDoodle(doodle));
 
         $httpBackend.flush();
     });
 
     it('get doodle for user should return an array of test doodles', function () {
         repository
-            .getDoodlesForUser(userId)
+            .getDoodlesForUser(Home.UnitTestCommon.RepositoryTest.userId)
             .then(doodles => {
                 expect(doodles.constructor === Array).toBe(true);
-                doodles.forEach(doodle => expectDoodle(doodle));
+                doodles.forEach(doodle => Home.UnitTestCommon.RepositoryTest.expectDoodle(doodle));
             });
 
         $httpBackend.flush();
@@ -208,7 +118,7 @@ describe('Repository', function () {
 
     it('delete doodle should return true', function () {
         repository
-            .deleteDoodle(doodle._id)
+            .deleteDoodle(Home.UnitTestCommon.RepositoryTest.doodle._id)
             .then(deleted => expect(deleted).toBe(true));
 
         $httpBackend.flush();
